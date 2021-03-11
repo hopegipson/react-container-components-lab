1cbd2151b43dd463491008208d1c5ed8a59b12d1
hopegipson <gipson.hope@gmail.com>
2021-03-10 19:13:26 -0600

    commit tree 41ec8ec880833084c21a57908c04819f10f5847e

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,16 @@
 // Code MovieReviews Here
+import React, { Component} from 'react';
+    const MovieReviews = (props) =>
+
+            <div className="review-list">
+            {props.reviews.map(review => (
+             <li className="review">
+                 {`${review.summary_short}`}
+           </li>
+            ))}
+        </div>
+
+    export default MovieReviews;
+
+        
+    
